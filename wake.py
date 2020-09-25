b2544baef011e594a357fa889ddc9dc38bd89b9f
Night Key <davidjantho@gmail.com>
2020-09-26 00:31:00 +0200

    set the delay to 2

--- wake.py
+++ wake.py
@@ -465,7 +465,7 @@ pcs.set_window(ui_update)
 check_loop = threading.Thread(target=loop)
 check_loop.name = "Wake check loop"
 check_loop.start()
-_api = API.API("Waker", "ef6a9df062560ce93e1236bce9dc244a6223f1f68ba3dd6a6350123c7719e78c")
+_api = API.API("Waker", "ef6a9df062560ce93e1236bce9dc244a6223f1f68ba3dd6a6350123c7719e78c", max_delay=2)
 _api.validate()
 _api.create_function("wake", "Wakes up the user's connected PC's\nCategory: NETWORK", UI_wake, API.SENDER)
 _api.create_function("shutdown", "shuts down the user's connected PC's\nCategory: NETWORK", shutdown_pc, API.SENDER)
