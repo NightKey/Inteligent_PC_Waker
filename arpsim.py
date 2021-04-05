04154c0c516b5efa1aa91c0ad4f9588febdf23ca
Night <davidjantho@gmail.com>
2021-04-05 18:52:16 +0200

    f

--- arpsim.py
+++ arpsim.py
@@ -6,7 +6,7 @@ import os
 
 mac_filter = r"([a-fA-F0-9]{2}[:-]){5}([a-fA-F0-9]{2})"
 ip_filter = r"([0-9]{1,3}[.]){3}([0-9]){1,3}"
-system_specific_switches = ["-n", "-a"] if system() == "Windows" else ["-c", ""]
+system_specific_switches = ["-n", ["arp", "-a"]] if system() == "Windows" else ["-c", ["arp"]]
 
 def pre_check(ip_range):
     devnull = open(os.devnull, "wb")
@@ -15,6 +15,6 @@ def pre_check(ip_range):
     devnull.close()
 
 def arp_scan():
-    reply = subprocess.check_output(["arp", system_specific_switches[1]])
+    reply = subprocess.check_output(system_specific_switches[1])
     reply = [[item for item in line.replace("\r", "").strip().split(' ') if re.match(mac_filter, item) or re.match(ip_filter, item)] for line in reply.decode("utf-8").split("\n") if re.search(mac_filter, line)]
     return reply
